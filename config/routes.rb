8673bbce85bc4be8b3c05483004e87f93424fa35
Rushaine <codekid24@gmail.com>
2012-10-26 19:32:35 -0400

    Creation of projects feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ticketeeme::Application.routes.draw do
+  root :to => "projects#index"
+
+  resources :projects
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
