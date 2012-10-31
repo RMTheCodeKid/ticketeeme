9a62a3e1bf60c2e75832ee78b2fc063550df802f
Rushaine <codekid24@gmail.com>
2012-10-30 23:52:09 -0400

    Don't show projects that a user doesn't have permission to see

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,8 +8,11 @@ class Project < ActiveRecord::Base
     joins(:permissions).where(:permissions => { :action => "view",
                                                 :user_id => user.id })
   }
-  
+
 
   attr_accessible :name
 
+  def self.for(user)
+    user.admin? ? Project : Project.readable_by(user)
+  end
 end
